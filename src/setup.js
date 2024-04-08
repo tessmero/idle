5882fa68e5cd30519fae80f989d19ce6f36eb557
Oliver Tessmer <oliver.tessmer@gmail.com>
2024-04-07 20:31:18 -0400

    fix typo

--- src/setup.js
+++ src/setup.js
@@ -28,7 +28,7 @@ function init() {
     document.addEventListener("keydown", keyDown )
     document.addEventListener("keyup", keyUp )
     
-    // document.querySelector('body').addEventListener('contextmenu', (event) => {
+    document.querySelector('body').addEventListener('contextmenu', (event) => {
       event.preventDefault();
     })
     global.canvas = cvs
